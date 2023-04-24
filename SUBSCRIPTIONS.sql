086233e6abc622d658d87176b054dfea8100ea18
tvannatt <129558302+tvannatt@users.noreply.github.com>
2023-04-23 23:35:08 -0400

    Add files via upload

---
+++ SUBSCRIPTIONS.sql
@@ -0,0 +1,52 @@
+
+DROP TABLE SUBSCRIPTIONS;
+
+CREATE TABLE SUBSCRIPTIONS 
+   ( 
+     CHANNEL_ID VARCHAR2(26), 
+	 CHANNEL_URL VARCHAR2(128), 
+	 CHANNEL_TITLE VARCHAR2(128)
+   ); 
+
+SET DEFINE OFF;
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UC2uUACGpICl-fG6mB2LyLIQ','http://www.youtube.com/channel/UC2uUACGpICl-fG6mB2LyLIQ','Pryoxis');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UC3JIDpjOFMKXObgctLRQb-A','http://www.youtube.com/channel/UC3JIDpjOFMKXObgctLRQb-A','Citizen Soldier');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UC3dfk4vg1tXLrmYG8ZNGW3A','http://www.youtube.com/channel/UC3dfk4vg1tXLrmYG8ZNGW3A','ian kung');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UC6E2mP01ZLH_kbAyeazCNdg','http://www.youtube.com/channel/UC6E2mP01ZLH_kbAyeazCNdg','Brave Wilderness');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UC7_YxT-KID8kRbqZo7MyscQ','http://www.youtube.com/channel/UC7_YxT-KID8kRbqZo7MyscQ','Markiplier');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCC5hxCe133rthsY96GVtDsQ','http://www.youtube.com/channel/UCC5hxCe133rthsY96GVtDsQ','Fanco Wrestling');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCDrG2_1TcVkXKXXsD6Kjwig','http://www.youtube.com/channel/UCDrG2_1TcVkXKXXsD6Kjwig','Michael Bisping');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCGCZBBvu7ZnqHYHuScODbAQ','http://www.youtube.com/channel/UCGCZBBvu7ZnqHYHuScODbAQ','Chewjitsu');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCHunBH1FCnxgfgBwFjIHUDg','http://www.youtube.com/channel/UCHunBH1FCnxgfgBwFjIHUDg','republicattak');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCMCMoFQ3ym2iaH2hcY0WpaA','http://www.youtube.com/channel/UCMCMoFQ3ym2iaH2hcY0WpaA','SparklyPants');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCN5xN8gIljvWYnsj06V8WbQ','http://www.youtube.com/channel/UCN5xN8gIljvWYnsj06V8WbQ','Pyrocynical');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCNvzD7Z-g64bPXxGzaQaa4g','http://www.youtube.com/channel/UCNvzD7Z-g64bPXxGzaQaa4g','gameranx');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCOA1yrrvuBsl0ifIioiTNXQ','http://www.youtube.com/channel/UCOA1yrrvuBsl0ifIioiTNXQ','ReformistTM');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCUz5B7duySZTBOo2Uln6kZA','http://www.youtube.com/channel/UCUz5B7duySZTBOo2Uln6kZA','ilkerrbr');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCXuqSBlHAE6Xw-yeJA0Tunw','http://www.youtube.com/channel/UCXuqSBlHAE6Xw-yeJA0Tunw','Linus Tech Tips');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCZ8kRL8S3O-XBdgirSUDiHQ','http://www.youtube.com/channel/UCZ8kRL8S3O-XBdgirSUDiHQ','Cayden Henschel');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCZiqc1Zq_1TYr8wRlCpx-Qg','http://www.youtube.com/channel/UCZiqc1Zq_1TYr8wRlCpx-Qg','Stefan Karl');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCb7DXZOHX3uvZSn3vlr9tTw','http://www.youtube.com/channel/UCb7DXZOHX3uvZSn3vlr9tTw','Bapelxrd');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCbJnFXI_NsehxVNPKZN3V1Q','http://www.youtube.com/channel/UCbJnFXI_NsehxVNPKZN3V1Q','GameKing956');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCc38g_k9LYTx3uvvYASGktw','http://www.youtube.com/channel/UCc38g_k9LYTx3uvvYASGktw','Minty');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCdYAt_kceIuiDwklp0EeIFw','http://www.youtube.com/channel/UCdYAt_kceIuiDwklp0EeIFw','Thalasso hobbyer 㿟ら㿿㿻㿳や');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCdoPCztTOW7BJUPk2h5ttXA','http://www.youtube.com/channel/UCdoPCztTOW7BJUPk2h5ttXA','Call Me Kevin');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCftcLVz-jtPXoH3cWUUDwYw','http://www.youtube.com/channel/UCftcLVz-jtPXoH3cWUUDwYw','Bitwit');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCgFvT6pUq9HLOvKBYERzXSQ','http://www.youtube.com/channel/UCgFvT6pUq9HLOvKBYERzXSQ','Davie504');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCi8PYovluicFUUD_YxJIcMA','http://www.youtube.com/channel/UCi8PYovluicFUUD_YxJIcMA','United World Wrestling');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCioNNjH3S7X8byCjPDEqZkA','http://www.youtube.com/channel/UCioNNjH3S7X8byCjPDEqZkA','Aviators');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCj1VqrHhDte54oLgPG4xpuQ','http://www.youtube.com/channel/UCj1VqrHhDte54oLgPG4xpuQ','Stuff Made Here');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCpJZz0NtSgIdFhaXSPV4YqQ','http://www.youtube.com/channel/UCpJZz0NtSgIdFhaXSPV4YqQ','Stanz');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCpqXJOEqGS-TCnazcHCo0rA','http://www.youtube.com/channel/UCpqXJOEqGS-TCnazcHCo0rA','theRadBrad');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCrPseYLGpNygVi34QpGNqpA','http://www.youtube.com/channel/UCrPseYLGpNygVi34QpGNqpA','Ludwig');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCshoKvlZGZ20rVgazZp5vnQ','http://www.youtube.com/channel/UCshoKvlZGZ20rVgazZp5vnQ','CaptainSparklez');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCsvn_Po0SmunchJYOWpOxMg','http://www.youtube.com/channel/UCsvn_Po0SmunchJYOWpOxMg','videogamedunkey');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCtApDZWXv8fQaxZAk6dxYkw','http://www.youtube.com/channel/UCtApDZWXv8fQaxZAk6dxYkw','Ghostcharm');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCtoaZpBnrd0lhycxYJ4MNOQ','http://www.youtube.com/channel/UCtoaZpBnrd0lhycxYJ4MNOQ','HasanAbi');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCudIlQLuz_BGxdQPJvjqiYA','http://www.youtube.com/channel/UCudIlQLuz_BGxdQPJvjqiYA','Kaiteido');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values ('UCynfaUytX_K-ElCmUrmQViA','http://www.youtube.com/channel/UCynfaUytX_K-ElCmUrmQViA','Scrambleman17');
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values (null,null,null);
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values (null,null,null);
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values (null,null,null);
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values (null,null,null);
+Insert into  SUBSCRIPTIONS (CHANNEL_ID,CHANNEL_URL,CHANNEL_TITLE) values (null,null,null);
